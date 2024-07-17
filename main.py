5bdcd3815cbba306252a39363b3585a0b364d1b2
Himanshu Shukla <638himanshushukla708@gmail.com>
2024-07-17 13:01:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,279 @@
+# # print("helkoef")
+# # print(5)
+# # print("Bye")
+# # print(7*5)
+# # print(15/5)
+# # print(15%7)
+# # print(15+5)
+# # print(15-5)
+# # a,b,c=8,2,3
+# # print(a)
+# # print(b)
+# # print(c)
+# # se1=se2=3
+# # print(se1)
+# # print(se2)/
+# # import calendar
+# # c = calendar.month(2354,7)
+# # print (c)
+# # a= 'himanshu'
+# # b='shujka'
+# # print(a+b)
+# a=10
+# b=10
+# # print(a<<b)
+
+
+
+# a=5
+# print("Type of a: ",type(a) ) #to find the data type 
+
+
+
+# a= True
+# print(type(a))
+
+
+
+
+# var = 100
+# if(var==100):print(" value of expression is 100")
+# print('goof voirsaf')
+
+
+
+# num = -10 
+# if(num>0):
+#     print("number is positive")
+# else:
+#     print("number is negative")
+# print('bingo')
+
+
+
+# num = eval(input("enter the number"))
+# if (num>0):
+#     print('+ve number')
+# elif(num==0):
+#     print("Zero")
+# else:
+#     print("-ve number")
+# print("Tingo bingo")    
+
+
+
+
+# num = -15
+# if(num>=0):
+#     if(num==0):
+#         print("number is Zero")
+#     else:
+#         print('number is +ve')
+# else:
+#     print("number is -ve")
+
+
+
+# num = int(input("Enter the number :"))
+# if (num%2==1):
+#     print("number id Odd")
+# else:
+#     print("number is Even")
+
+
+# num = int(input("Enter the number :"))
+# if (num%2==0):
+#     print("number id Even")
+# else:
+#     print("number is Odd")
+
+
+
+# a = 'himanshu'
+# for i in a:
+#     print(i)
+
+
+
+# languages =  ("String", 7,"tuple")
+# for i in languages:
+#     print(i)
+
+
+# a  = 'Himanshu sudhanshu '
+# for i in a:
+#     print(i)
+
+
+
+
+# dict={1:"a",2:"b",3:"c"}
+# for i,k in dict.items():
+#     print(i,k)
+
+
+
+# i=1
+# n=89
+
+# while i<=n:
+#     print(i)
+#     i=i+1
+    
+
+
+# for i in range (0,9,1):
+#     for j in range(i):
+#         print(i, end='')
+#     print()
+
+
+
+
+# number= [1,2,3,4,5,6,7,8,9]
+# target=7
+# for i in number:
+#     if (i == target):
+#         print(target)
+#         break
+#     print(i)
+# print("loop is finshed")
+
+
+
+
+# count=1
+# while True:
+#     print(count)
+#     count= count +1
+#     if (count>=9009):
+#         break
+# print(9009)
+# print("we got the numbers")    
+
+
+# range(1,11)
+# for i in range(1,10):
+#     if (i %2==0):
+#         continue
+#     print(i)
+
+
+
+# num = [1,2,3,4,5,6,7,8,9,10]
+# for i in num:
+#     if (i%2==0):
+#         continue
+#     print(i)
+# print('this will always run')
+
+
+
+# myList='abcdefgh'
+# print(myList[1])
+
+
+# g = 'HELLO'
+# print(g[-1])
+
+
+
+# g='HELLO'
+# print(g[0:3])
+
+
+
+# g = 'msg'
+# g= 'binod'
+# print(g)
+
+
+
+# num1= eval(input("Enter the 1st number  :"))
+# num2=eval(input("Enter the 2nd number  :"))
+# a = num1+num2
+# b = num1*num2
+# c = num1-num2
+# print(a)
+# print(b)
+# print(c)
+
+
+
+# n='hello '
+# g='himNSHU'
+# r=n + g
+# print(r)
+
+
+# n='HELLO'
+# for i in n:
+#     print(i)
+
+
+
+
+# l1=[1,2,3]
+# l2=['a','b','c']
+# my_list = [1,2,3,4,5]
+
+# contatenated_list= l1 + l2
+# print(contatenated_list)
+
+# repeated_list= l1 * 3
+# print(repeated_list)
+
+# length = len(my_list)
+# print(length)
+
+# print(2 in my_list)
+
+
+
+# l = [4,2,3,5,1]
+
+# l.append(6)
+# print(l)
+
+# l.remove(3)
+# print(l)
+
+# l.sort()
+# print(l)
+
+
+
+# # tuple packing
+
+# tuple= 1,2,'a'
+# print(tuple)
+
+# # tuple unpacking
+
+# a,b,c,=tuple
+# print(a)
+# print(b)
+# print(c)
+
+
+
+
+# dict= {'name':'Ram', 'age': 27, 'city':'Ayodhya'}
+
+# dict['age']= 29
+
+# dict['gender']= 'male'
+
+# print(dict)
+
+
+
+
+
+# dict= {'name':'Ram', 'age': 27, 'city':'Ayodhya'}
+
+# del dict['city']
+
+# dict.pop('age')
+# print(dict)
+
